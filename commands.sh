e10402a0bcc947f9a75b887cf9b50c6285f78c11
Cristian Reyes <cristian@debian-host.debian-domain>
2022-07-03 22:22:19 -0300

    installing devise

--- commands.sh
+++ commands.sh
@@ -253,6 +253,147 @@ postgres=# \l
                    |          |          |             |             | postgres=CTc/postgres
 (6 rows)
 
+# setting up devise
 
+$ bundle add devise
+Fetching gem metadata from https://rubygems.org/..........
+Resolving dependencies...
+Fetching gem metadata from https://rubygems.org/..........
+Resolving dependencies...
+Using rake 13.0.6
+Using builder 3.2.4
+Using crass 1.0.6
+Using racc 1.6.0
+Using websocket-extensions 0.1.5
+Using marcel 1.0.2
+Using digest 3.1.0
+Using concurrent-ruby 1.1.10
+Using bindex 0.8.1
+Using bundler 2.3.17
+Using matrix 0.4.2
+Using mini_mime 1.1.2
+Using io-console 0.5.11
+Using timeout 0.3.0
+Using strscan 3.0.3
+Using public_suffix 4.0.7
+Using msgpack 1.5.3
+Using minitest 5.16.2
+Using rack 2.2.4
+Using nio4r 2.5.8
+Using regexp_parser 2.5.0
+Using childprocess 4.1.0
+Using erubi 1.10.0
+Using method_source 1.0.0
+Using thor 1.2.1
+Fetching orm_adapter 0.5.0
+Fetching bcrypt 3.1.18
+Using rexml 3.2.5
+Using rubyzip 2.3.2
+Using websocket 1.2.9
+Using nokogiri 1.13.6 (x86_64-linux)
+Using websocket-driver 0.7.5
+Using bootsnap 1.12.0
+Using rack-test 2.0.2
+Using zeitwerk 2.6.0
+Using sprockets 4.1.1
+Using puma 5.6.4
+Using selenium-webdriver 4.3.0
+Using net-protocol 0.1.3
+Using addressable 2.8.0
+Using i18n 1.10.0
+Using tzinfo 2.0.4
+Using net-pop 0.1.1
+Using net-smtp 0.3.1
+Using pg 1.4.1
+Using reline 0.3.1
+Using loofah 2.18.0
+Using irb 1.4.1
+Using webdrivers 5.0.0
+Using net-imap 0.2.3
+Fetching warden 1.2.9
+Using mail 2.7.1
+Using activesupport 7.0.3
+Using xpath 3.2.0
+Using rails-dom-testing 2.0.3
+Using activemodel 7.0.3
+Using capybara 3.37.1
+Using rails-html-sanitizer 1.4.3
+Using debug 1.5.0
+Using actionview 7.0.3
+Using activerecord 7.0.3
+Using globalid 1.0.0
+Using actionpack 7.0.3
+Using activejob 7.0.3
+Using sprockets-rails 3.4.2
+Using jbuilder 2.11.5
+Using actioncable 7.0.3
+Using railties 7.0.3
+Using activestorage 7.0.3
+Using actionmailer 7.0.3
+Using actionmailbox 7.0.3
+Using actiontext 7.0.3
+Using stimulus-rails 1.0.4
+Using turbo-rails 1.1.1
+Using importmap-rails 1.1.2
+Using web-console 4.2.0
+Fetching responders 3.0.1
+Using rails 7.0.3
+Installing warden 1.2.9
+Installing responders 3.0.1
+Installing bcrypt 3.1.18 with native extensions
+Installing orm_adapter 0.5.0
+Fetching devise 4.8.1
+Installing devise 4.8.1
 
+$ rails g devise:install
+      create  config/initializers/devise.rb
+      create  config/locales/devise.en.yml
+# ===============================================================================
+#
+# Depending on your application's configuration some manual setup may be required:
+#
+#   1. Ensure you have defined default url options in your environments files. Here
+#      is an example of default_url_options appropriate for a development environment
+#      in config/environments/development.rb:
+#
+#        config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+#
+#      In production, :host should be set to the actual host of your application.
+#
+#      * Required for all applications. *
+#
+#   2. Ensure you have defined root_url to *something* in your config/routes.rb.
+#      For example:
+#
+#        root to: "home#index"
+#
+#      * Not required for API-only Applications *
+#
+#   3. Ensure you have flash messages in app/views/layouts/application.html.erb.
+#      For example:
+#
+#        <p class="notice"><%= notice %></p>
+#        <p class="alert"><%= alert %></p>
+#
+#      * Not required for API-only Applications *
+#
+#   4. You can copy Devise views (for customization) to your app by running:
+#
+#        rails g devise:views
+#
+#      * Not required *
+#
+# ===============================================================================
+
+# Creating User model
+
+$ rails g devise User role:integer
+      invoke  active_record
+      create    db/migrate/20220704012035_devise_create_users.rb
+      create    app/models/user.rb
+      invoke    test_unit
+      create      test/models/user_test.rb
+      create      test/fixtures/users.yml
+      insert    app/models/user.rb
+       route  devise_for :users
 
