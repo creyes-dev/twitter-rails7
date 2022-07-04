407b1557f7c351137382551e983a6802743d97a2
Cristian Reyes <cristian@debian-host.debian-domain>
2022-07-03 21:00:05 -0300

    first commit

---
+++ commands.sh
@@ -0,0 +1,36 @@
+psql and deployment
+https://www.youtube.com/watch?v=5QUTfcO7BEw
+
+setup solargraph
+https://www.youtube.com/watch?v=mkLDPpDSWng
+
+commit inicial
+
+$ git status
+On branch main
+Your branch is up to date with 'origin/main'.
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	Gemfile
+	Gemfile.lock
+	Rakefile
+	app/
+	bin/
+	commands.sh
+	config.ru
+	config/
+	db/
+	lib/
+	log/
+	public/
+	storage/
+	test/
+	tmp/
+	vendor/
+
+nothing added to commit but untracked files present (use "git add" to track)
+
+
+
+
