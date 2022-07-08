60e81f8ab26eb13f2317a43a29066186eaa51dc0
Cristian Reyes <cristian@debian-host.debian-domain>
2022-07-08 01:24:51 -0300

    creating group registers

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,13 @@ CompetitionStructure.create!(description: "league and tournament playoff")
 
 Competition.create!(name: "Mundial qatar 2022", competition_structure_id: 3, national_teams: true, groups: 8, teams_group: 4, rounds: 1,  begin: "2022-11-21T09:00:00+00:00", active: true)
 
+Group.create!(competition_id: 1, name: "A", rounds: 1)
+Group.create!(competition_id: 1, name: "B", rounds: 1)
+Group.create!(competition_id: 1, name: "C", rounds: 1)
+Group.create!(competition_id: 1, name: "D", rounds: 1)
+Group.create!(competition_id: 1, name: "E", rounds: 1)
+Group.create!(competition_id: 1, name: "F", rounds: 1)
+Group.create!(competition_id: 1, name: "G", rounds: 1)
+Group.create!(competition_id: 1, name: "H", rounds: 1)
+
 
