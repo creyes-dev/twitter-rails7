29dbf1b5b8194de692ff31b9f08a1d71cc2d41e9
creyes-dev <creyes.dev@gmail.com>
2022-07-30 14:50:34 -0300

    adding competing teams matches relationship

--- app/models/competing_team.rb
+++ app/models/competing_team.rb
@@ -2,4 +2,11 @@ class CompetingTeam < ApplicationRecord
   belongs_to :group
   belongs_to :team, optional: true
   has_one :competing_team_score
+  has_many :home_matches, class_name: "Match", foreign_key: "competing_team_1"
+  has_many :away_matches, class_name: "Match", foreign_key: "competing_team_2"
+
+  # Get all matches
+  def matches
+    self.home_matches + self.away_matches
+  end
 end
