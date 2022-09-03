064c29748631f0f46ce0e50450f824e93b4ea95b
creyes-dev <creyes.dev@gmail.com>
2022-09-03 14:41:11 -0300

    adding roles enum to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
+  enum role: [:user, :admin]
+
   validates :name, presence: true, length: {maximum:50}
   validates :first_name,  presence: true, length: {maximum:50}
   validates :last_name, presence: true, length: {maximum:50}
