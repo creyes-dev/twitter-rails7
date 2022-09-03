4ad32bcab1af810e24372225def1dfcc8fc89c59
creyes-dev <creyes.dev@gmail.com>
2022-09-03 16:08:22 -0300

    only signed in users can access dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :department_id, :password, :password_confirmation])
   end
+
+  def logged_in_user
+    if current_user.nil?
+      flash[:danger] = "Please log in."
+      redirect_to new_user_session_path
+    end
+  end
+
 end
