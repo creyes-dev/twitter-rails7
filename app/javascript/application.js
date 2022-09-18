bc8387a2a654f5a02e0f7ec9145c32fad1248194
creyes-dev <creyes.dev@gmail.com>
2022-09-18 13:13:10 -0300

    removing theme js logic to inline functions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,19 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-import "./src/jquery"
-
 $(function() {
     console.log("Hello from jQuery!");
     });
 
+import "./src/jquery"
 import "./src/global.min.js"
+
 import "./src/chart.bundle.min.js"
 import "./src/jquery.nice-select.js"
 import "./src/apexchart.js"
 
-import "./src/moment.js"
-import "./src/bootstrap-datetimepicker.min.js"
-
-window.importmapScriptsLoaded = true;
-console.log(window.importmapScriptsLoaded);
