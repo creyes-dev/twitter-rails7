6575d39c291305851d84ab4e77d28774be53998a
creyes-dev <creyes.dev@gmail.com>
2022-10-24 02:44:49 -0300

    conditional matches order by fixed

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -9,7 +9,6 @@ class MatchesController < ApplicationController
 
     # TODO: This could be more elegant
     is_finished = (params[:is_finished] == '1' ? true : false)
-    is_finished_order = "matches.datetime " + (params[:is_finished] == '1' ? "ASC" : "DESC")
     limit = (params[:limit] == '0' ? 490490 : params[:limit].to_i)
     round_condition = (params[:round_id] == '0' ? "1=1" : "matches.round_id = #{params[:round_id]}")
 
@@ -25,8 +24,7 @@ class MatchesController < ApplicationController
     .where( competition: { active: true })
     .where('is_finished = ?', is_finished )
     .where(round_condition)
-    .order(datetime: :desc, name: :desc)
+    .order(is_finished ? { datetime: :desc} : { datetime: :asc }, is_finished ? { name: :desc } : { name: :asc})
     .limit(limit)
   end
-  # left_outer_joins ,        :match_stadistics
 end
